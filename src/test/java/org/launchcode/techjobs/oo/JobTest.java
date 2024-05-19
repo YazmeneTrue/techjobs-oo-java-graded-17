40e77334d5e9e1984c5a9ca054538f316401d556
Yazzy Boo <yazzyboo@Chazzs-MacBook-Pro.local>
2024-05-19 15:42:32 -0400

    test the empty constructor

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,32 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+
 public class JobTest {
     //TODO: Create your unit tests here
+@Test
+    public void testSettingJobId(){
+    Job demoJob1 = new Job();
+    Job demoJob2 = new Job();
+
+    assertNotEquals(demoJob1.getId(), demoJob2.getId());
 }
+@Test
+    public void testJobConstructorSetsAllFields(){
+   Job demoJob = new Job("Product tester", new Employer("ACME"),
+            new Location("Desert"), new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+
+   assertEquals("Product tester", demoJob.getName());
+   assertEquals("ACME", demoJob.getEmployer().getValue());
+   assertEquals("Desert", demoJob.getLocation().getValue());
+   assertEquals("Quality control", demoJob.getPositionType().getValue());
+   assertEquals("Persistence", demoJob.getCoreCompetency().getValue());
+
+
+}
+
+    }
